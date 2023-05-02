84f60c78fd4c5f020f27118f2b4b2203c17bab96
Amarjeet <kumaramarjeetraj71@gmail.com>
2023-05-02 15:04:36 +0530

    First commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -36,6 +36,44 @@ export default function TextForm(props) {
         props.showAlert("Extra spaces removed!", "success");
     }
 
+    // const handleExtractNumber = () => {
+    //     let newText = text.split(/[ ]+/);
+    //     setText(newText.join(" "));
+    //     props.showAlert("Extra spaces removed!", "success");
+    // }
+    
+    const handleReverse = (event) => {
+        /* Convert string to array*/
+        let strArr = text.split("");
+        /* Reverse array*/
+        strArr = strArr.reverse();
+        /* Convert array to string*/
+        let newText = strArr.join("");
+        setText(newText);
+        props.showAlert("Text Reversed!", "success")
+      };
+
+
+      function textToSpeech(){
+        const Speech= new SpeechSynthesisUtterance();
+        const message= document.getElementById("myBox").value;
+        Speech.lang='eng';
+        Speech.text= message;
+        window.speechSynthesis.speak(Speech);
+      }
+    
+
+    //this function converts the first letter of every word to uppercase the rest of letters are lower case
+const handleCapitalizeWordClick = () => {
+    let lowercase = text.toLowerCase();
+    let words = lowercase.split(" ");
+    let newWords = words.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    let newText = newWords.join(" ");
+    setText(newText);
+  };
+
     const [text, setText] = useState(''); 
     return (
         <>
@@ -45,11 +83,15 @@ export default function TextForm(props) {
             <div className="mb-3"> 
             <textarea className="form-control" value={text} onChange={handleOnChange1} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text </button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCapitalizeWordClick}>Case Sensetive </button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick1}>Convert to Uppercase </button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick1}>Convert to Lowercase </button>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text </button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy1}>Copy Text</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces1}>Remove Extra Spaces</button>
+            
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleReverse}>Reverse Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={textToSpeech}>&#127908; Speak Text </button>
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
@@ -57,6 +99,7 @@ export default function TextForm(props) {
             <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Nothing to preview!"}</p>
+
         </div>
         </>
     )
