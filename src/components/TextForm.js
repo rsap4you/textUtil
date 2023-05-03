9a08d7939d46cf641dead352cd0ec2231ddde1ee
Amarjeet <kumaramarjeetraj71@gmail.com>
2023-05-03 10:30:58 +0530

    First commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -76,7 +76,7 @@ export default function TextForm(props) {
     
 
     //this function converts the first letter of every word to uppercase the rest of letters are lower case
-const handleCapitalizeWordClick = () => {
+ const handleCapitalizeWordClick = () => {
     let lowercase = text.toLowerCase();
     let words = lowercase.split(" ");
     let newWords = words.map((word) => {
@@ -85,6 +85,23 @@ const handleCapitalizeWordClick = () => {
     let newText = newWords.join(" ");
     setText(newText);
   };
+
+
+  let [count, setCount] = useState(0);
+  let countChar = 0;
+  
+  const handleVoClick = () => {
+    for (count = 0; count <= text.length; count++) {
+      if (text.charAt(count).match(/[aeiouAEIOU]/)) {
+        countChar++;
+        setCount (countChar);
+        // const result = countVowel(string);   
+        props.showAlert("Count Vowels !", "success");
+      }
+    }
+    // console.log("No. of Vowels are: " + countChar);
+  };
+
     
     const [text, setText] = useState(''); 
     return (
@@ -96,7 +113,7 @@ const handleCapitalizeWordClick = () => {
             <textarea className="form-control" value={text} onChange={handleOnChange1} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text </button>
-            {/* <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={countVowel}>countVowel </button> */}
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleVoClick}>countVowel </button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCapitalizeWordClick}>Case Sencetive </button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick1}>Convert to Uppercase </button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick1}>Convert to Lowercase </button>
@@ -112,7 +129,8 @@ const handleCapitalizeWordClick = () => {
             <h2>Your text summary</h2>
             <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
             <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
-           
+            <p style={{color: "red"}}>Count Vowels </p> 
+            <p>{count}</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Nothing to preview!"}</p>
 
